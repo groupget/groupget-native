6af3f587e280e3512b08098f05f3b43ded2e70a9
Irmina Krysiak <irmina.krysiak@gmail.com>
2019-04-08 10:38:36 +0200

    Typescript configuration & files reorganization.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,13 +1,11 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
 import renderer from 'react-test-renderer';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
+import App from '../App';
 
 describe('App snapshot', () => {
   jest.useFakeTimers();
   beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
   });
 
   it('renders the loading screen', async () => {
