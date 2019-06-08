13398b8dd08e4590e8a2025eadc02b87cd6f0550
Irmina Krysiak <irmina.krysiak@gmail.com>
2019-06-08 10:50:32 +0200

    Assets fix.

--- App.js
+++ App.js
@@ -103,10 +103,6 @@ class App extends React.Component {
 
     _loadResourcesAsync = async () => {
         return Promise.all([
-            Asset.loadAsync([
-                require('./assets/images/robot-dev.png'),
-                require('./assets/images/robot-prod.png'),
-            ]),
             Font.loadAsync({
                 ...Icon.Ionicons.font,
                 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
